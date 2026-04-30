9c79dbcf0e000b6fc1153e09c3841b16763f4e70
steffhan <steffhan@oregonstate.edu>
2026-04-30 09:05:03 -0700

    Add applicaion source and Dockerfile

---
+++ test_app.py
@@ -0,0 +1,9 @@
+from app import app
+
+
+def test_index():
+    # Flask's built-in test client 
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.get_json()["status"] == "ok"
